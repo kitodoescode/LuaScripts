7822246a9dd7a9de80314a49a697f1a7c2371d9a
kito-code <156444563+KKGithubUser99@users.noreply.github.com>
2025-03-09 18:21:53 +0530

    Update spoofx.lua

--- spoofx.lua
+++ spoofx.lua
@@ -2,10 +2,10 @@
 local function spoofx(string spoofname, int version)
   local x = identifyexecutor;
   local y = getexecutorname;
-  local hook = hookfunction(x, function() -- use hook to return the spoofed name when the function "identifyexecutor()" is detected
+  local hook = hookfunction(x, function() -- use hook to return the spoofed name and version when the function "identifyexecutor()" is received
     print(spoofname, version) -- prints the spoofed name
   end)
-  local hook2 = hookfunction(y, function() -- use hook to return the spoofed name when the function "identifyexecutor()" is detected
+  local hook2 = hookfunction(y, function() -- use hook to return the spoofed name when the function "getexecutorname()" is received
     print(spoofname) -- prints the spoofed name
   end)
 end
